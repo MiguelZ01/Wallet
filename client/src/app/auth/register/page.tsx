64cfb5511c54ee44e1b9c1abb6e7f293ae30c491
Felipe Moriones <96366130+Moriones20@users.noreply.github.com>
2023-08-29 23:07:15 -0500

    [Login] Modified styles and added rules in format code

--- client/src/app/auth/register/page.tsx
+++ client/src/app/auth/register/page.tsx
@@ -4,52 +4,65 @@ import Auth from '@assets/Auth.png';
 
 export default function Register() {
   return (
-    <div className='flex w-screen h-screen my-auto'>
-      <div className='flex flex-col w-[60%] bg-[#000000] p-28 px-56'>
-        <h1 className='text-center mb-8'>REGISTRO</h1>
+    <div className="flex w-screen h-screen my-auto">
+      <div className="flex flex-col w-[60%] bg-[#000000] p-28 px-56">
+        <h1 className="text-center mb-8">REGISTRO</h1>
         <form action="">
-        <div className='flex flex-col mb-4'>
-          <label htmlFor="email">
-            Nombre
-          </label>
-          <input className='bg-Button rounded h-8 p-2 text-black' type="text" name='email'/>
-        </div>
+          <div className="flex flex-col mb-4">
+            <label htmlFor="email">Nombre</label>
+            <input
+              className="bg-Button rounded h-8 p-2 text-black"
+              type="text"
+              name="email"
+            />
+          </div>
 
-        <div className='flex flex-col mb-4'>
-          <label htmlFor="email">
-            Email
-          </label>
-          <input className='bg-Button rounded h-8 p-2 text-black' type="email" name='email'/>
-        </div>
+          <div className="flex flex-col mb-4">
+            <label htmlFor="email">Email</label>
+            <input
+              className="bg-Button rounded h-8 p-2 text-black"
+              type="email"
+              name="email"
+            />
+          </div>
 
-        <div className='flex flex-col mb-4'>
-          <label htmlFor="email">
-            Contraseña
-          </label>
-          <input className='bg-Button rounded h-8 p-2 text-black' type="password" name='email'/>
-        </div>
+          <div className="flex flex-col mb-4">
+            <label htmlFor="email">Contraseña</label>
+            <input
+              className="bg-Button rounded h-8 p-2 text-black"
+              type="password"
+              name="email"
+            />
+          </div>
 
-        <div className='flex flex-col mb-8'>
-          <label htmlFor="email">
-            Confirmar contraseña
-          </label>
-          <input className='bg-Button rounded h-8 p-2 text-black' type="password" name='email'/>
-        </div>
+          <div className="flex flex-col mb-8">
+            <label htmlFor="email">Confirmar contraseña</label>
+            <input
+              className="bg-Button rounded h-8 p-2 text-black"
+              type="password"
+              name="email"
+            />
+          </div>
 
-        <div className='flex justify-between'>
-          <button type='submit' className='border border-solid border-20 border-MainYellow text-MainYellow w-[30%] rounded h-8 font-medium mb-2'> 
-            Cancelar
-          </button>
+          <div className="flex justify-between">
+            <button
+              type="submit"
+              className="border border-solid border-20 border-MainYellow text-MainYellow w-[30%] rounded h-8 font-medium mb-2"
+            >
+              Cancelar
+            </button>
 
-          <button type='submit' className='bg-MainYellow w-[30%] text-black rounded h-8 font-medium mb-2'> 
-            Registrar
-          </button>
-        </div>
-
+            <button
+              type="submit"
+              className="bg-MainYellow w-[30%] text-black rounded h-8 font-medium mb-2"
+            >
+              Registrar
+            </button>
+          </div>
         </form>
       </div>
-      <div className='flex w-[40%] bg-[#ffa500]'>
-        <Image className='w-[70%] h-[40%]' src={Auth} alt='Auth' width='100'/>
+      <div className="flex w-[40%] bg-[#ffa500]">
+        <Image className="w-[70%] h-[40%]" src={Auth} alt="Auth" width="100" />
       </div>
     </div>
   );
---
+++ client/src/app/auth/social_callback/failure/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Failure() {
+  return (
+    <div>
+      <h1>Failre auth</h1>
+    </div>
+  );
+}
