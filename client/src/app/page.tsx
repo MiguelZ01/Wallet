abf7d9b302b1602da2f5dc216d457f6c0179ba3f
MiguelZ01 <jesusmigu3lzr2004@gmail.com>
2023-09-01 17:39:32 -0500

    Conexion

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -17,17 +17,21 @@ export default function HomePage() {
       </div>
       <div className="h-full w-[46%] flex flex-col justify-center pr-40">
         <h1 className="font-extrabold text-7xl">Welcome to</h1>
-        <h1 className="font-extrabold text-7xl text-MainYellow my-2">e-Wallet!</h1>
+        <h1 className="font-extrabold text-7xl text-MainYellow my-2">
+          e-Wallet!
+        </h1>
         <p>
           Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dignissimos,
           aut? Numquam maxime eos asperiores enim sunt nobis, nulla deleniti
           praesentium fuga, incidunt vero in. Deleniti iure doloremque
           voluptatum amet nemo.
         </p>
-        <Link href={'/auth/login'} prefetch={false} className='bg-MainYellow w-[40%] p-2 my-4 rounded text-black font-semibold flex justify-center'>
-          <button>
-            Log In
-          </button>
+        <Link
+          href={'/auth/login'}
+          prefetch={false}
+          className="bg-MainYellow w-[40%] p-2 my-4 rounded text-black font-semibold flex justify-center"
+        >
+          <button>Log In</button>
         </Link>
       </div>
     </div>
